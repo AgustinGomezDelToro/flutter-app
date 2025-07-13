4554ea5a33db94db7ed543b10e0471d327941cdb
Yanis Benhagouga <74181063+HBerg75@users.noreply.github.com>
2025-07-13 07:51:32 +0200

    [FIX] Fix api url to use android emulator

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,6 +1,4 @@
-const String baseUrl = String.fromEnvironment(
-  'API_BASE_URL',
-  defaultValue: 'http://localhost:4000',
-);
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+final String baseUrl = dotenv.env['API_BASE_URL'] ?? 'http://10.0.2.2:4000';
 final Uri baseUri = Uri.parse(baseUrl);
